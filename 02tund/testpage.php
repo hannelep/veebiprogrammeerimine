5e5c428f5dd1e9da948ce799f1326f171b0db7d2
Hannele Pruunlep <hannelep@users.noreply.github.com>
2019-09-10 11:09:21 +0300

    Esimene üleslaadimine

---
+++ 02tund/testpage.php
@@ -0,0 +1,32 @@
+<?php
+  $userName = "Hannele Pruunlep";
+?>
+<!DOCTYPE html>
+<html lang="et">
+<head>
+  <meta charset="utf-8">
+  <title>
+  <?php
+  echo $userName;
+  ?>
+   programmeerib veebi</title>
+  
+  
+</head>
+<body>
+  <?php
+  echo "<h1>" .$userName .", veebiprogrammeerimine</h1>";
+  ?>
+  <h2 style="color:red">Oluline!</h2>
+  <p title="Tõesti väga oluline"> See veebileht on loodud õppetöö käigus ning ei sisalda mingit tõsiseltvõetavat sisu!</p>
+  <hr>
+  
+  <h2 style="color:purple">Minu tunnid teisipäeval on:</h2>
+  <ul>
+    <li>Veebiprogrammeerimine</li>
+	<li>Andmebaaside projekteerimine</li>
+	<li>Operatsioonisüsteemide alused ja haldamine</li>
+  
+
+</body>
+</html>
