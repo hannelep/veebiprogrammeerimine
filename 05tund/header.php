3b139206c034b771f5eac404329cb94a2a20af38
Hannele Pruunlep <hannelep@users.noreply.github.com>
2019-10-01 11:37:03 +0300

    kasutaja lisamine

--- 05tund/header.php
+++ 05tund/header.php
@@ -1,11 +1,10 @@
 <?php
   $userName = "Hannele Pruunlep";
 	
- 
   $pageHeaderHTML = "<!DOCTYPE html> \n";
   $pageHeaderHTML .= '<html lang="et">'. "\n";
   $pageHeaderHTML .= "<head> \n";
-  $pageHeaderHTML .= "\t" .'<meta charset="utf-8">' ."\n \t<title>" .$userName ."programmeerib veebi</title> \n";
+  $pageHeaderHTML .= "\t" .'<meta charset="utf-8">' ."\n \t<title>" .$userName ." programmeerib veebi</title> \n";
   $pageHeaderHTML .= "</head> \n";
   $pageHeaderHTML .= "<body>";
   echo $pageHeaderHTML;
